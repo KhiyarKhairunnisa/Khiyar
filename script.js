8d89c040be27f81223e6ab4e49adffa4967efcc8
Khiyar Khairunnisa <jungwoonaa@gmail.com>
2025-03-19 16:05:13 +0700

    Update script.js

--- script.js
+++ script.js
@@ -2,9 +2,6 @@ function toggleMode() {
     const body = document.body;
     body.classList.toggle("dark-mode");
   }
-  
-  <div id="loader" style="position:fixed;width:100%;height:100%;background:#042698;color:white;display:flex;align-items:center;justify-content:center;font-size:24px;z-index:9999;">Anda akan dialihkan...</div>
-
 window.onload = () => {
     setTimeout(() => {
         document.getElementById("loader").style.display = "none";
