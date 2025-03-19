70a11a551f6e5509011076e8776e9608c7df5b49
Khiyar Khairunnisa <jungwoonaa@gmail.com>
2025-03-19 11:20:54 +0700

    Update script.js

--- script.js
+++ script.js
@@ -1,2 +1,12 @@
-// fungsi untuk menampilkan halo
-alert ("Halo! Selamat Datang");
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  }
+  
+  <div id="loader" style="position:fixed;width:100%;height:100%;background:#042698;color:white;display:flex;align-items:center;justify-content:center;font-size:24px;z-index:9999;">Anda akan dialihkan...</div>
+
+window.onload = () => {
+    setTimeout(() => {
+        document.getElementById("loader").style.display = "none";
+    }, 1500); // Loader 1.5 detik
+};
